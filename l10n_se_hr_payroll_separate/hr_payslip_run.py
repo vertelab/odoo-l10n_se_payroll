1ecabe808c48d4596f9710c75cd57d835d3e1125
Anders Wallenquist <anders.wallenquist@vertel.se>
2016-12-29 02:37:55 +0100

    Merge

--- l10n_se_hr_payroll_separate/hr_payslip_run.py
+++ l10n_se_hr_payroll_separate/hr_payslip_run.py
@@ -36,7 +36,7 @@ except:
 import logging
 _logger = logging.getLogger(__name__)
 
-<<<<<<< HEAD
+
 
 class hr_attendance(models.Model):
     _inherit = 'hr.attendance'
@@ -68,7 +68,7 @@ class hr_attendance(models.Model):
         self._constraints = [(lambda f: True, 'Error ! Sign in (resp. Sign out) must follow Sign out (resp. Sign in)', ['action'])]
         raise Warning(self._constraints)
         super(hr_attendance, self)._auto_init(cr, context)
-=======
+
 class base_synchro(models.TransientModel):
     _inherit = 'base.synchro'
     
@@ -98,98 +98,8 @@ class base_synchro(models.TransientModel):
                             'obj_id': sync_obj.id,
                             'name': '1900-01-01 00:00:00',
                         })
->>>>>>> 107e45bfaa707ab23f72391771972bfe729e7934
 
-class hr_payslip_run(models.Model):
-    _inherit = 'hr.payslip.run'
 
-    @api.model
-    def generate_csv(self):
-        temp = tempfile.NamedTemporaryFile(mode='w+t',suffix='.csv')
-        rules = self.env['hr.salary.rule'].search([('active', '=', True), ('appears_on_payslip', '=', True)], order='sequence, salary_art')
-        labelwriter = None
-        for slip in self.slip_ids.sorted(key=lambda s: s.employee_id.contract_id.name):
-            if not labelwriter:
-                columns = ['Anst nr', 'Name']
-                labelwriter = csv.DictWriter(temp, columns + ['%s\n%s' %(r.name.replace(u'ö', 'o').replace(u'å', 'a').replace(u'ä', 'a').encode('ascii', 'ignore'), r.salary_art) for r in rules if r.salary_art])
-                labelwriter.writeheader()
-            labelwriter.writerow(slip.get_payslip_run_row(rules))
-        temp.seek(0)
-        self.env['ir.attachment'].create({
-            'name': self.name.replace(' ', '_') + '.csv',
-            'res_name': self.name,
-            'res_model': self._name,
-            'res_id': self.id,
-            'datas': base64.encodestring(temp.read()),
-            'datas_fname': self.name.replace(' ', '_') + '.csv',
-        })
-        temp.close()
-        return True
 
-    @api.one
-    def _company_id(self):
-        self.company_id = self.env['res.company'].browse(self.env['res.users'].browse(self._uid)._get_company())
-    company_id = fields.Char(compute="_company_id")
-
-    # fields match Arbetsgivardeklaration document from Swelon.se
-    @api.one
-    def _nbr_employee(self):
-        self.nbr_employee = len(self.slip_ids)
-    nbr_employee = fields.Integer(compute="_nbr_employee")
-
-    @api.one
-    def _taxable_salary(self): #50
-        self.taxable_salary = sum([s.get_slip_line('bl')[0]['total'] for s in self.slip_ids if s.get_slip_line('bl')])
-    taxable_salary = fields.Integer(compute="_taxable_salary")
-
-    @api.one
-    def _social_normal_fees(self): #55 & 56
-        self.fully_social_normal_fees = sum([s.get_slip_line('san')[0]['amount'] for s in self.slip_ids if s.get_slip_line('san')])
-        self.social_normal_fees = sum([s.get_slip_line('san')[0]['total'] for s in self.slip_ids if s.get_slip_line('san')])
-    fully_social_normal_fees = fields.Integer(compute="_social_normal_fees")
-    social_normal_fees = fields.Integer(compute="_social_normal_fees")
-
-    @api.one
-    def _social_youth_fees(self): #57 & 58
-        self.fully_social_youth_fees = sum([s.get_slip_line('sau')[0]['amount'] for s in self.slip_ids if s.get_slip_line('sau')])
-        self.social_youth_fees = sum([s.get_slip_line('sau')[0]['total'] for s in self.slip_ids if s.get_slip_line('sau')])
-    fully_social_youth_fees = fields.Integer(compute="_social_youth_fees")
-    social_youth_fees = fields.Integer(compute="_social_youth_fees")
-
-    @api.one
-    def _social_retired_fees(self): #59 & 60
-        self.fully_social_retired_fees = sum([s.get_slip_line('sap')[0]['amount'] for s in self.slip_ids if s.get_slip_line('sap')])
-        self.social_retired_fees = sum([s.get_slip_line('sap')[0]['total'] for s in self.slip_ids if s.get_slip_line('sap')])
-    fully_social_retired_fees = fields.Integer(compute="_social_retired_fees")
-    social_retired_fees = fields.Integer(compute="_social_retired_fees")
-
-    @api.one
-    def _total_tax(self): #82/88
-        self.total_tax = sum([s.get_slip_line('pre')[0]['total'] for s in self.slip_ids if s.get_slip_line('pre')])
-    total_taxable = fields.Integer(compute="_total_tax")
-
-    @api.one
-    def _general_payroll_tax(self): #70/78
-        self.general_payroll_tax = sum([s.get_slip_line('sa')[0]['total'] for s in self.slip_ids if s.get_slip_line('sa')])
-    general_payroll_tax = fields.Integer(compute="_general_payroll_tax")
-
-    @api.one
-    def _net_salary(self): #81
-        self.net_salary = sum([s.get_slip_line('net')[0]['total'] for s in self.slip_ids if s.get_slip_line('net')])
-    net_salary = fields.Integer(compute="_net_salary")
-
-class hr_payslip(models.Model):
-    _inherit = 'hr.payslip'
-
-    @api.model
-    def get_payslip_run_row(self, rules):
-        rec = {
-            'Anst nr': self.employee_id.contract_id.name.encode('utf-8') if self.employee_id.contract_id else 'N/A',
-            'Name': self.employee_id.name.encode('utf-8'),
-        }
-        for r in rules:
-            if r.salary_art and self.get_slip_line(r.code):
-                rec['%s\n%s' %(r.name.replace(u'ö', 'o').replace(u'å', 'a').replace(u'ä', 'a').encode('ascii', 'ignore'), r.salary_art)] = self.get_slip_line(r.code)[0]['total']
-        return rec
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
