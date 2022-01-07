08b1fb2064e7db61570ab10c0d4022045d6b888a
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-01-07 16:28:48 +0100

    Update __manifest__.py

--- l10n_se_hr_payroll_ku10/__manifest__.py
+++ l10n_se_hr_payroll_ku10/__manifest__.py
@@ -1,27 +1,39 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# OpenERP, Open Source Management Solution, third party addon
-# Copyright (C) 2004-2015 Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 {
-    'name': 'Swedish - Payroll KU10',
+    'name': 'l10n_se_payroll: KU10',
+    'version': '14.0.1.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Swedish benefits, accounting codes.',
     'category': 'Localization',
+    #'sequence': '1'
+    'images': ['/static/description/banner.png'], # 560x280 px.
     'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-l10n-se-payroll/l10n-se_payroll_ku10',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-l10n-se-payroll',
     'depends': ['l10n_se_hr_payroll','l10n_se_hr_payroll_benefits', 'report_glabels'],
     'version': '1.2',
     'description': """
@@ -56,7 +68,6 @@ Kostnadsersattningar (salary rules/input fields)
     """,
 
     'auto_install': False,
-    'website': 'http://www.vertel.se',
     'data':[
         'report_ku10_view.xml',
     ],
