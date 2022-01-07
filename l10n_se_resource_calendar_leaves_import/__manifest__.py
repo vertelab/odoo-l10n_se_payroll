e3e1d543feca0eecb53f800dfe52f9c8955f0cd7
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-01-07 16:57:31 +0100

    Update __manifest__.py

--- l10n_se_resource_calendar_leaves_import/__manifest__.py
+++ l10n_se_resource_calendar_leaves_import/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution, third party addon
-#    Copyright (C) 2004-2017 Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,20 +15,29 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 {
-    'name': "Swedish Holidays Schedule",
+    'name': 'l10n_se_payroll: Swedish Holidays Schedule',
+    'version': '14.0.1.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Import swedish holidays to work schedule.',
-    'description': """""",
-    'author': "Vertel AB",
-    'website': "http://www.vertel.se",
+    'category': 'Localization',
+    'description': '',
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-l10n-se-payroll/l10n-se_resource_calendar_leaves_import',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-l10n-se-payroll',
+
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
     'depends': ['resource_calendar_leaves_import'],
     'data': [
         'resource_data.xml',
