0a4c108f8468b47bbe30d2119988bcfc2ba41769
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-01-07 16:01:39 +0100

    Update __manifest__.py

--- l10n_se_hr_holidays/__manifest__.py
+++ l10n_se_hr_holidays/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Enterprise Management Solution, third party addon
-#    Copyright (C) 2014- Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,17 +15,27 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 {
-    'name': 'Swedish - Holidays',
-    'category': 'Localization',
-    'author': 'Vertel AB',
-    'depends': ['l10n_se_hr_payroll','hr_holidays'],
-    'version': '1.0',
-    'licence': 'AGPL-3',
+    'name': 'l10n_se_payroll: Holidays',
+    'version': '14.0.1.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Swedish Holidays Rules',
+    'category': 'Localization',
+    #'sequence': '1'
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-l10n-se-payroll/l10n-se_holidays',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-l10n-se-payroll',
+    'depends': ['l10n_se_hr_payroll','hr_holidays'],
+    'licence': 'AGPL-3',
     'description': """
 Swedish Holidays Rules
 ======================
@@ -38,10 +48,8 @@ Swedish Holidays Rules
 * Holiday earning rules can add days to an employees hr.holidays.earning
   from hr.payslip
 
-
-    """,
+""",
     'auto_install': False,
-    'website': 'http://vertel.se',
     'data':[
         'views/hr_employee_view.xml',
         'views/hr_holidays_view.xml',
