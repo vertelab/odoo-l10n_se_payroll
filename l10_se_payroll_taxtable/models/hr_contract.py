aebcf7a5c18ca32a25dfb2c10cc16789ad83f501
Marcus Jakobsson <jakmar@odooutv14.vertel.se>
2022-03-24 12:56:30 +0000

    l10_se_payroll_taxtable:Fixed payrules

--- l10_se_payroll_taxtable/models/hr_contract.py
+++ l10_se_payroll_taxtable/models/hr_contract.py
@@ -1,17 +1,20 @@
 import urllib.request
 import pandas as pd
 from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 from datetime import datetime
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class HRContract(models.Model):
     _inherit = 'hr.contract'
 
     table_number = fields.Integer(string="Tax Table")
     is_church_deductible = fields.Boolean(string="Deductible")
-    column_number = fields.Many2one('ir.model.fields', domain=[
-        ('model_id.model', '=', 'payroll.taxtable.line'), ('ttype', '=', 'float')
-    ])
+    # ~ column_number = fields.Many2one('ir.model.fields', domain=[
+        # ~ ('model_id.model', '=', 'payroll.taxtable.line'), ('ttype', '=', 'float')
+    # ~ ])
+    column_number = fields.Selection([ ('column1', "Column 1"),('column2', "Column 2"),('column3', "Column 3"),('column4', "Column 4"),('column5', "Column 5"),('column6', "Column 6")],'Tax Column')
 
     def action_sync_taxable(self):
         taxable_url = self.env['ir.config_parameter'].sudo().get_param('taxable_url')
@@ -44,11 +47,23 @@ class HRContract(models.Model):
             pass
 
     def l10_sum_columns_taxtable_line(self, date, wage):
-        year = datetime.strptime(date, '%M/%d/%Y').year
-        taxable_lines = self.env['payroll.taxtable.line'].search([
+
+        fails = [key for key, value in (('date', date), ('wage', wage), ('column_number', self.column_number), ('self.table_number', self.table_number)) if not value]
+        if fails:
+            _logger.warning(f"Please fill these values{fails}")
+            return
+            
+        year = date.year
+        taxable_line = self.env['payroll.taxtable.line'].search([
             ('table_number', '=', self.table_number),
-            ('income_from', '>=', wage),
-            ('income_to', '<=', wage),
-            ('year', '=', str(year))
+            ('income_from', '<=', float(wage)),
+            ('income_to', '>=', float(wage)),
+            ('year', '=', year)
         ])
-        return sum([line[self.column_number].name for line in taxable_lines])
+        
+        return getattr(taxable_line, self.column_number)
+     
+        # ~ _logger.warning(f"{self.table_number}")
+        # ~ _logger.warning(f"{wage}")
+        # ~ _logger.warning(f"{taxable_lines}")
+        # ~ return sum([line[self.column_number].name for line in taxable_lines])
