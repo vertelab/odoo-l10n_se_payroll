19a501de9065d4bbe0de7e6911ad1d3dc7132c55
iammiracle <miracleaayodele@gmail.com>
2022-03-25 11:50:04 +0100

    payroll taxable changes

--- l10_se_payroll_taxtable/models/hr_contract.py
+++ l10_se_payroll_taxtable/models/hr_contract.py
@@ -14,34 +14,14 @@ class HRContract(models.Model):
     ])
 
     def action_sync_taxable(self):
-        taxable_url = self.env['ir.config_parameter'].sudo().get_param('taxable_url')
-        taxable_file = urllib.request.urlopen(taxable_url)
-        file_name = taxable_file.info().get_filename()
-
-        try:
-            data = pd.read_csv(taxable_url, encoding='latin-1', sep=";")
-            data_dict = data.to_dict('index')
-            taxable_id = self.env['payroll.taxtable'].create({
-                'name': file_name
-            })
-            for tuple_vals in data_dict.values():
-                self.env['payroll.taxtable.line'].create({
-                    'year': tuple_vals.get('År'),
-                    'number_of_days': tuple_vals.get('Antal dgr'),
-                    'table_number': 0 if tuple_vals.get('Tabellnr') == 'null' else tuple_vals.get('Tabellnr'),
-                    'income_from': 0 if tuple_vals.get('Inkomst fr.o.m.') == 'null' else tuple_vals.get(
-                        'Inkomst fr.o.m.'),
-                    'income_to': 0 if tuple_vals.get('Inkomst t.o.m.') == 'null' else tuple_vals.get('Inkomst t.o.m.'),
-                    'column1': 0 if tuple_vals.get('Kolumn 1') == 'null' else float(tuple_vals.get('Kolumn 1')),
-                    'column2': 0 if tuple_vals.get('Kolumn 2') == 'null' else float(tuple_vals.get('Kolumn 2')),
-                    'column3': 0 if tuple_vals.get('Kolumn 3') == 'null' else float(tuple_vals.get('Kolumn 3')),
-                    'column4': 0 if tuple_vals.get('Kolumn 4') == 'null' else float(tuple_vals.get('Kolumn 4')),
-                    'column5': 0 if tuple_vals.get('Kolumn 5') == 'null' else float(tuple_vals.get('Kolumn 5')),
-                    'column6': 0 if tuple_vals.get('Kolumn 6') == 'null' else float(tuple_vals.get('Kolumn 6')),
-                    'payroll_taxable_id': taxable_id.id,
-                })
-        except Exception as e:
-            pass
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'payroll.taxtable.wizard',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': self.env.ref('l10_se_payroll_taxtable.view_payroll_taxtable_wizard_form').id,
+            'target': 'new',
+        }
 
     def l10_sum_columns_taxtable_line(self, date, wage):
         year = datetime.strptime(date, '%M/%d/%Y').year
--- l10_se_payroll_taxtable/models/payroll_taxable.py
+++ l10_se_payroll_taxtable/models/payroll_taxable.py
@@ -1,4 +1,43 @@
+import urllib.request
+import pandas as pd
 from odoo import models, fields, api, _
+from datetime import datetime
+
+
+class PayrollTaxableWizard(models.Model):
+    _name = "payroll.taxtable.wizard"
+
+    taxable_url = fields.Char(string="Taxable URL")
+
+    def action_sync_taxable(self):
+        taxable_url = self.env['ir.config_parameter'].sudo().get_param('taxable_url')
+        taxable_file = urllib.request.urlopen(taxable_url)
+        file_name = taxable_file.info().get_filename()
+
+        try:
+            data = pd.read_csv(taxable_url, encoding='latin-1', sep=";")
+            data_dict = data.to_dict('index')
+            taxable_id = self.env['payroll.taxtable'].create({
+                'name': file_name
+            })
+            for tuple_vals in data_dict.values():
+                self.env['payroll.taxtable.line'].create({
+                    'year': tuple_vals.get('År'),
+                    'number_of_days': tuple_vals.get('Antal dgr'),
+                    'table_number': 0 if tuple_vals.get('Tabellnr') == 'null' else tuple_vals.get('Tabellnr'),
+                    'income_from': 0 if tuple_vals.get('Inkomst fr.o.m.') == 'null' else tuple_vals.get(
+                        'Inkomst fr.o.m.'),
+                    'income_to': 0 if tuple_vals.get('Inkomst t.o.m.') == 'null' else tuple_vals.get('Inkomst t.o.m.'),
+                    'column1': 0 if tuple_vals.get('Kolumn 1') == 'null' else float(tuple_vals.get('Kolumn 1')),
+                    'column2': 0 if tuple_vals.get('Kolumn 2') == 'null' else float(tuple_vals.get('Kolumn 2')),
+                    'column3': 0 if tuple_vals.get('Kolumn 3') == 'null' else float(tuple_vals.get('Kolumn 3')),
+                    'column4': 0 if tuple_vals.get('Kolumn 4') == 'null' else float(tuple_vals.get('Kolumn 4')),
+                    'column5': 0 if tuple_vals.get('Kolumn 5') == 'null' else float(tuple_vals.get('Kolumn 5')),
+                    'column6': 0 if tuple_vals.get('Kolumn 6') == 'null' else float(tuple_vals.get('Kolumn 6')),
+                    'payroll_taxable_id': taxable_id.id,
+                })
+        except Exception as e:
+            pass
 
 
 class PayrollTaxable(models.Model):
