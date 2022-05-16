12f445dd2a03d4de0b721405b86d1c4052cd2c63
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-05-16 15:35:13 +0200

    Update __manifest__.py

--- l10_se_payroll_taxtable/__manifest__.py
+++ l10_se_payroll_taxtable/__manifest__.py
@@ -25,7 +25,7 @@
     'summary': '',
     'category': 'Localization',
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-l10n-se-payroll/l10n-se_payroll_taxablet',
+    'website': 'https://vertel.se/apps/odoo-l10n-se-payroll/l10n-se_payroll_taxable',
     'license': 'AGPL-3',
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-l10n-se-payroll',
