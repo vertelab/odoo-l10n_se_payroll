c786d1f6d493e9fc34cd0329b7a1bc853f9f2d4e
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-05-17 09:57:29 +0200

    Update __manifest__.py

--- l10n_se_hr_holidays_account/__manifest__.py
+++ l10n_se_hr_holidays_account/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'l10n_se_payroll: Holiday Pay with Accounting',
     'version': '14.0.1.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'summary': '',
+    'summary': 'Accounting Data for Swedish Holiday Pay Rules.',
     'category': 'Localization',
     #'sequence': '1'
     'author': 'Vertel AB',
