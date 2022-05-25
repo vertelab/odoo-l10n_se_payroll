787fbd2f2832be63c47d628ad89f5851271f3cdf
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-05-25 15:36:43 +0200

    Update __manifest__.py

--- l10n_se_resource_calendar_leaves_import/__manifest__.py
+++ l10n_se_resource_calendar_leaves_import/__manifest__.py
@@ -28,7 +28,7 @@
     'description': '',
     #'sequence': '1'
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-l10n-se-payroll/l10n-se_resource_calendar_leaves_import',
+    'website': 'https://vertel.se/apps/l10n_se_payroll/l10n_se_resource_calendar_leaves_import',
     'images': ['/static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
