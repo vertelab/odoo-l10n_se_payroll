096e6ff78681df7dbbe1b2d8baaf4f513f002776
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-05-25 14:56:57 +0200

    Update __manifest__.py

--- l10n_se_export/__manifest__.py
+++ l10n_se_export/__manifest__.py
@@ -28,7 +28,7 @@
     'description': '',
     #'sequence': '1'
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-l10n-se-payroll/l10n-se_export',
+    'website': 'https://vertel.se/apps/l10n_se_payroll/l10n_se_export',
     'images': ['/static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
