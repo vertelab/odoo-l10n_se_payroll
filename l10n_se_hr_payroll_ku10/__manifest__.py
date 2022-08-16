8bbf6930feffbcc9264a3d08a566566625576cfd
Jakob Krabbe <jakob.krabbe@vertel.se>
2022-08-16 09:38:07 +0200

    l10n_se_hr_payroll_ku10: change order of dependencies'

--- l10n_se_hr_payroll_ku10/__manifest__.py
+++ l10n_se_hr_payroll_ku10/__manifest__.py
@@ -33,7 +33,7 @@
     'contributor': '',
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-l10n-se-payroll',
-    'depends': ['l10n_se_hr_payroll','l10n_se_hr_payroll_benefits', 'report_glabels'],
+    'depends': ['l10n_se_hr_payroll_benefits', 'report_glabels', 'l10n_se_hr_payroll'],
     'description': """
 Swedish Payroll Rules.
 ======================
