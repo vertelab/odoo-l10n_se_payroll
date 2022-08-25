d4a5b64e3995fbd5926e8a06a7fd5af0a2420b37
Marcus Jakobsson <84468251+MarkusJacobsson@users.noreply.github.com>
2022-08-25 16:31:51 +0200

    Update __manifest__.py

--- l10n_se_payroll_taxtable/__manifest__.py
+++ l10n_se_payroll_taxtable/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'l10n_se_payroll: Payroll Tax Table',
     'version': '14.0.1.0.0',
-    'summary': '',
+    'summary': 'https://skatteverket.entryscape.net/store/9/resource/1534',
     'category': 'Localization',
     'author': 'Vertel AB',
     'website': 'https://vertel.se/apps/l10n_se_payroll/l10n_se_payroll_taxtable',
