93b89692405fb4d819c3362d45fe860d3375f6a9
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-19 11:21:19 +0200

    Update __manifest__.py

--- l10n_se_hr_payroll_employeefund/__manifest__.py
+++ l10n_se_hr_payroll_employeefund/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'l10n_se_payroll: Payroll Employee Fund',
     'version': '14.0.1.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'summary': '',
+    'summary': 'Employee benefits. Swedish payroll employee fund.',
     'category': 'Payroll Localization',
     #'sequence': '1'
     'author': 'Vertel AB',
