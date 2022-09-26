5aac9c43ea7e5c526b2715c56cf67f47f822d3ac
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-26 13:41:31 +0200

    Update __manifest__.py

--- l10n_se_payslip/__manifest__.py
+++ l10n_se_payslip/__manifest__.py
@@ -1,13 +1,32 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 # © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-        'name': 'l10n_se Payroll: Paysplip',
-    'version': '1.0.0',
+    'name': 'l10n_se Payroll: Paysplip',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Sample py3o payslip reports.',
     'category': 'Payroll Localization',
-    'license': 'AGPL-3',
-    'summary': 'Sample py3o payslip reports',
     'description': """
 HR Expense Report Py3o
 ======================
@@ -15,7 +34,14 @@ HR Expense Report Py3o
 This module adds a sample py3o expense reports.
 
     """,
+    #'sequence': '1',
     'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-l10n_se_payroll',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-l10n_se_payroll',
     'depends': [
         'report_py3o',
         'l10n_se_hr_payroll',
@@ -23,3 +49,4 @@ This module adds a sample py3o expense reports.
     'data': ['report.xml'],
     'installable': True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
