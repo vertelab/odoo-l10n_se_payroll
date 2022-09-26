c8828af205c06f61f5ebd37c470ae999a9ab9fa9
Jakob Krabbe <jakob.krabbe@vertel.se>
2022-09-26 13:34:50 +0200

    minor changes in manifestfile

--- l10n_se_payslip/__manifest__.py
+++ l10n_se_payslip/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-        'name': 'l10n_se Payroll Paysplip',
+        'name': 'l10n_se Payroll: Paysplip',
     'version': '1.0.0',
     'category': 'Payroll Localization',
     'license': 'AGPL-3',
