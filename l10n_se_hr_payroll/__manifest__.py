c5c3ed959315af6dbeb9f5aa7483a653676071df
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-02-07 16:45:06 +0100

    Update __manifest__.py

--- l10n_se_hr_payroll/__manifest__.py
+++ l10n_se_hr_payroll/__manifest__.py
@@ -34,10 +34,9 @@
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-l10n-se-payroll',
     # ~ 'depends': ['payroll','hr_contract_types','account_period','hr','hr_timesheet_sheet','hr_weekly_working_hours'],
-    'depends': [
-        'payroll','account_period','hr','hr_timesheet_sheet'
+    'depends': [ 'payroll','account_period','hr','hr_timesheet_sheet',]
         # 'hr_weekly_working_hours'
-        ], # hr_contract_types | CybroOdoo-OpenHRMS/hr_contract_types
+        # hr_contract_types | CybroOdoo-OpenHRMS/hr_contract_types
     'description': """
 Swedish Payroll Rules.
 ======================
