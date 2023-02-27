04e7ac955398bfeda5d119d861c1c3d9f711ccb1
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-02-27 16:03:52 +0100

    Update __manifest__.py
    spelling error

--- l10n_se_hr_payroll_benefits/__manifest__.py
+++ l10n_se_hr_payroll_benefits/__manifest__.py
@@ -30,7 +30,7 @@ Swedish Payslip additions for Benefits
     """,
     #'sequence': '1'
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/l10n_se_payroll/l10n_se_hr_payroll_benefits',
+    'website': 'https://vertel.se/apps/odoo-l10n_se_payroll/l10n_se_hr_payroll_benefits',
     'images': ['static/description/banner.png'],  # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
