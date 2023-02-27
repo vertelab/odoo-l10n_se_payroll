26b93f8e389a55288b96b7317fb7e8033c13e6c5
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-02-27 16:05:00 +0100

    Update __manifest__.py
    spelling error

--- l10n_se_hr_payroll_credit_coupling/__manifest__.py
+++ l10n_se_hr_payroll_credit_coupling/__manifest__.py
@@ -25,7 +25,7 @@
     'summary': 'Extends payroll with a field that links between payslips and credited payslips',
     'category': 'Payroll Localization',
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/l10n_se_payroll/l10n_se_hr_payroll_credit_coupling',
+    'website': 'https://vertel.se/apps/odoo-l10n_se_payroll/l10n_se_hr_payroll_credit_coupling',
     'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
@@ -46,3 +46,4 @@ the "account_period" module.
     ],
     'installable': True
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
