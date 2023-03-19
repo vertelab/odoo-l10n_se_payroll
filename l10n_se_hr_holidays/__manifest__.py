e26fb73d248907a26a5c569bf969a9dfc3dbebd5
Anders Wallenquist <anders.wallenquist@vertel.se>
2023-03-19 16:26:01 +0000

    l10n_se_hr_holidays: new salary rules l10n_se_hr_payroll_account: accounts now correct l10n_se_payroll_taxtable: now it works when the tax is cached, but it got an error when trying to fetch new taxes

--- l10n_se_hr_holidays/__manifest__.py
+++ l10n_se_hr_holidays/__manifest__.py
@@ -33,7 +33,7 @@
     'contributor': '',
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-l10n-se-payroll',
-    'depends': ['l10n_se_hr_payroll', 'hr_holidays', 'hr_weekly_working_hours', ],
+    'depends': ['l10n_se_hr_payroll', 'hr_holidays', 'hr_weekly_working_hours','l10n_se_payroll_taxtable' ],
     'licence': 'AGPL-3',
     'description': """
 Swedish Holidays Rules
