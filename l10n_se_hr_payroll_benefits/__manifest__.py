5dcf599426faedbc8b9cceb07b6c11aa601c522f
marcuszxc <marcus.karlsson@vertel.se>
2025-10-09 07:25:25 +0000

    l10n_se_hr_payroll_benefits: ported from 14 to 18 T/5947

---
+++ l10n_se_hr_payroll_benefits/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'l10n_se_payroll: Payroll with Benefits',
+    'version': '1.1',
+    'summary': 'Swedish Payslip additions for Benefits',
+    'category': 'Payroll Localization',
+    'description': """
+Swedish Payslip additions for Benefits
+    """,
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-l10n_se_payroll/l10n_se_hr_payroll_benefits',
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-l10n-se-payroll',
+    'depends': ['l10n_se_hr_payroll', 'hr_payroll_benefits'],
+    'data': [
+        'data/hr_salary_rule_data.xml',
+    ],
+    'auto_install': True,
+    'installable': True,
+}
