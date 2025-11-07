df2f88e528ff276144e2bc20bff0d0160d2112ba
marcuszxc <marcus.karlsson@vertel.se>
2025-11-07 13:01:50 +0000

    l10n_se_hr_payroll_tiichri: Added sick leave data to data files T/5947

--- l10n_se_hr_payroll_tiichri/__manifest__.py
+++ l10n_se_hr_payroll_tiichri/__manifest__.py
@@ -47,7 +47,6 @@ We create test data to seven employees, to use while developing our own code. To
         'data/data_05_gustav.xml',
         'data/data_06_helmer.xml',
         'data/data_07_karin.xml',
-        'data/data_10_aronssons_montage.xml',
     ],
     'auto_install': False,
     'post_init_hook': 'try_load_k2',
