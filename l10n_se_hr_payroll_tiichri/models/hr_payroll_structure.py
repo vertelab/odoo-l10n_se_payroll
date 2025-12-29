80a9dd5193aadd0a2a6eac2d289ef72a3e2addb6
JamieZiegler <james.ziegler@vertel.se>
2025-12-29 10:13:36 +0000

    l10n_se_hr_payroll_tiichri: Added constants and updated rules accordingly + fixed inconsistent names

--- l10n_se_hr_payroll_tiichri/models/hr_payroll_structure.py
+++ l10n_se_hr_payroll_tiichri/models/hr_payroll_structure.py
@@ -6,13 +6,14 @@ class HrPayrollStructure(models.Model):
     const_ids = fields.One2many(comodel_name = "hr_payroll.structure.const", inverse_name = "structure_id", string = "Constant")
 
     def get_constant(self, code):
-        pass
-    #mapped const_ids.filtered, (lambda c: c.code == code)
-    #return filtered
+        #pass
+        filtered_constants = self.const_ids.filtered(lambda c: c.code == code).mapped('constant')
+        return filtered_constants[0] if filtered_constants else 0.0
 
 
 class HrPayrollStructureConst(models.Model):
     _name = "hr_payroll.structure.const"
     structure_id = fields.Many2one(comodel_name = "hr.payroll.structure", string = "Structure")
+    name = fields.Char(string = "Name")
     constant = fields.Float(string = "Constant")
     code = fields.Char(string = "Code")
