b934341800a62cf2fe60794067bbac93f2a7672f
Vertel AB <vertel@vertel.se>
2026-06-02 22:20:52 +0000

    [18.0] fix date_end related field on hr.payslip.run - use period_id.date_stop instead of non-existent date_end

--- l10n_se_hr_payroll/models/hr_payslip_run.py
+++ l10n_se_hr_payroll/models/hr_payslip_run.py
@@ -21,7 +21,7 @@ class HrPayslipRun(models.Model):
         check_company=True,
     )
     date_start = fields.Date(related="period_id.date_start", store=True)
-    date_end = fields.Date(related="period_id.date_end", store=True)
+    date_end = fields.Date(related="period_id.date_stop", store=True)
 
     slip_ids_count = fields.Integer(
         string="Payslips Count",
@@ -31,24 +31,9 @@ class HrPayslipRun(models.Model):
     def _compute_slip_ids_count(self):
         for run in self:
             run.slip_ids_count = len(run.slip_ids)
-    name = fields.Char(required=True)
-
-    period_id = fields.Many2one(
-        comodel_name='account.period', 
-        string="Period",
-        required=True,
-        default=lambda self: self.env['account.period'].date2period(fields.Date.today()),
-        tracking=1, 
-        check_company=True
-    )
-    
-    date_start = fields.Date(related="period_id.date_start",store=True,default=None)
-    date_end = fields.Date(related="period_id.date_end",store=True,default=None )
 
     @api.onchange('period_id')
     def onchange_name(self):
-        _logger.error(f"{self.date_start=}")
-        _logger.error(f"{self.date_end=}")
         self.name = _("Salary batch for %s") % (
             self.period_id.date_start.strftime('%B-%Y') if self.period_id else 'None',
         )
